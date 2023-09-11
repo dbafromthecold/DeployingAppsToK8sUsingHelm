d070e5b74b7444c528e224363d4690073300d1ca
Andrew Pruski <dbafromthecold@gmail.com>
2023-09-11 11:15:24 +0100

    updated demos

--- demos/1. Deploying a Chart to Kubernetes.ps1
+++ demos/1. Deploying a Chart to Kubernetes.ps1
@@ -83,3 +83,73 @@ helm install nginx dbafromthecold/nginx
 
 # confirm deployment
 helm list
+
+
+
+# view status of release
+helm status nginx
+
+
+
+# view kubernetes objects
+kubectl get all
+
+
+
+# view the container image in the deployment
+kubectl get deployment -o jsonpath='{ .items[*].spec.template.spec.containers[*].image }' && echo ""
+
+
+
+# find nginx version in the pod
+POD=$(kubectl get pods -o jsonpath="{.items[0].metadata.name}") && echo $POD
+kubectl exec $POD -- nginx -v
+
+
+
+# test nginx
+IpAddress=$(kubectl get service nginx --no-headers -o custom-columns=":status.loadBalancer.ingress[*].hostname") && echo $IpAddress
+#IpAddress=$(kubectl get service nginx --no-headers -o custom-columns=":status.loadBalancer.ingress[*].ip") && echo $IpAddress
+curl $IpAddress
+
+
+
+# view release history
+helm history nginx
+
+
+
+# uninstall a release
+helm uninstall nginx --keep-history
+
+
+
+# view release history
+helm history nginx
+
+
+
+# confirm
+helm list
+
+
+
+# confirm with --all flag
+helm list --all
+
+
+
+# view objects
+kubectl get all
+
+
+
+# delete release
+helm delete nginx
+
+
+
+# confirm with --all flag
+helm list --all
+
+
